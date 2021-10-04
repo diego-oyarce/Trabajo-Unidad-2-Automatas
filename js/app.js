ba3c6ff429db89d842004d3f301c60254cf078f8
diego-oyarce <doyarce@utem.cl>
2021-10-04 20:35:35 -0300

    avance en diseño index.html y automatas.html 2.0

---
+++ js/app.js
@@ -0,0 +1,205 @@
+//VARIABLES PARA AUTOMATA 1
+var alfabetoAutomata1 = [];
+var alfabeto1 = [];
+var nodes = new vis.DataSet([]);
+var edges = new vis.DataSet([]);
+var arrayIdEstados = [];
+var arrayLabelEstados = [];
+var arrayEventos = [];
+var arrayEstadosFinales = [];
+var container = document.getElementById('automataDiv');
+var data = {
+  nodes: nodes,
+  edges: edges
+};
+var estadosVacios = [];
+
+//LOCALES PARA TODOS LOS AUTOMATAS
+var locales = {
+    en: {
+        edit:"Editar",
+        del:"Eliminar selección",
+        back:"Volver",
+        addNode:"Agregar Estado",
+        addEdge:"Agregar Transición",
+        editNode:"Editar Nodo",
+        editEdge:"Editar Arista",
+        addDescription:"Click en un espacio vacío para agregar un estado",
+        edgeDescription:"Click sobre estado-origen y arrastrar hasta estado-final para conectarlos",
+        editEdgeDescription:""
+    }
+};
+
+//OPTIONS PARA AUTOMATA 1
+var options = {
+    autoResize:true,
+    height:'100%',
+    width:'100%',
+    locale: 'en',
+    locales: locales,
+    clickToUse: true,
+    configure:{
+        enabled:false,
+        filter: 'nodes,edges',
+        showButton:true
+    },
+    edges:{
+        length:180,
+        arrows: {
+          to: {
+            enabled: true,
+            scaleFactor: 1,
+            type: "arrow"
+          },
+          middle: {
+            enabled: true,
+            scaleFactor: 1,
+            type: "image"
+          },
+          from: {
+            enabled: false,
+            scaleFactor: 1,
+            type: "arrow"
+          }
+        },
+    },
+    interaction:{
+        dragNodes:true,
+        dragView:true,
+        hover:true,
+        multiselect:true,
+        navigationButtons:false
+    },
+    manipulation:{
+        enabled:true,
+        initiallyActive: true,
+        addNode: function(data, callback){
+            var span = document.getElementById('operation-state');
+            var nodeSaveButton = document.getElementById('saveButton-state');
+            var nodeCancelButton = document.getElementById('cancelButton-state');
+            var node_div = document.getElementById('state-popUp');
+            span.innerHTML = "Añadir Estado";
+            nodeSaveButton.onclick = nodeSaveData.bind(this,data,callback);
+            nodeCancelButton.onclick = nodeClearPopUp.bind();
+            node_div.style.display = 'block';
+        },
+        addEdge: function(data, callback){
+            var edgeSpan = document.getElementById('operation-transition');
+            var edgeSaveButton = document.getElementById('saveButton-transition');
+            var edgeCancelButton = document.getElementById('cancelButton-transition');
+            var edge_div = document.getElementById('transition-popUp');
+            edgeSpan.innerHTML = "Añadir Transición";
+            edgeSaveButton.onclick = edgeSaveData.bind(this,data,callback);
+            edgeCancelButton.onclick = edgeClearPopUp.bind();
+            edge_div.style.display = 'block';
+        }, 
+        editEdge:false,   
+        deleteNode:true,
+        deleteEdge:false
+    }
+};
+
+//AUTOMATA 1
+var network = new vis.Network(container, data, options);
+
+//GUARDAR ALFABETO AUTOMATA 1
+function guardarAlfabeto(){
+    alfabetoAutomata1 = document.getElementById('alfabeto-automata1').value;
+    alfabeto1 = alfabetoAutomata1.split(';');
+    alfabeto1.sort();
+    alert("Alfabeto ingresado correctamente. Alfabeto: " + alfabeto1);
+}
+
+//A1
+function nodeClearPopUp() {
+    var nodeSaveButton = document.getElementById('saveButton-state');
+    var nodeCancelButton = document.getElementById('cancelButton-state');
+    nodeSaveButton.onclick = null;
+    nodeCancelButton.onclick = null;
+    var node_div = document.getElementById('state-popUp');
+    node_div.style.display = 'none';
+}
+
+//A1
+function nodeSaveData(data,callback) {
+    var nodeIdInput = document.getElementById('state-id');
+    var finalState = document.getElementById('state-final').value;
+    data.id = nodeIdInput.value;
+    if(alfabeto1.length == 0){
+      alert("CUIDADO! Aun no se ha ingresado el alfabeto. Ingresa el alfabeto y vuelve a intentarlo");
+      return null;
+    }
+    else{
+      if((finalState == 'si') || (finalState == 'SI') || (finalState == 'Si') || (finalState == 'no') || (finalState == 'NO') || (finalState == 'No')){
+        if(nodes.length == 0){
+          if(finalState == 'si'){
+            arrayIdEstados.push(nodes.length);
+            nodes.add({id:nodes.length, label:nodeIdInput.value+": Estado Inicial y Estado Final", title:"Estado Inicial y Final"});
+            arrayEstadosFinales.push(1);
+            arrayLabelEstados.push(data.id);
+          }
+          else{
+            arrayIdEstados.push(nodes.length);
+            nodes.add({id:nodes.length, label:nodeIdInput.value + ": Estado Inicial", title:"Estado Inicial"});
+            arrayEstadosFinales.push(0);
+            arrayLabelEstados.push(data.id);
+          }
+        }
+        else{
+          if(finalState == 'si'){
+            arrayIdEstados.push(nodes.length);
+            nodes.add({id:nodes.length, label:nodeIdInput.value + ": Estado Final", title:"Estado Final"});
+            arrayEstadosFinales.push(1);
+            arrayLabelEstados.push(data.id);
+          }
+          else{
+            arrayIdEstados.push(nodes.length);
+            nodes.add({id:nodes.length, label:nodeIdInput.value});
+            arrayEstadosFinales.push(0);
+            arrayLabelEstados.push(data.id);
+          }
+        }
+        nodeClearPopUp();
+        callback(data);
+      }
+      else{
+        alert("Se debe indicar si el estado ingresado es final o no. Ingrese 'si' o 'no'.");
+        return null;
+      }
+    }
+}
+
+//A1
+function edgeClearPopUp(){
+    var edgeSaveButton = document.getElementById('saveButton-transition');
+    var edgeCancelButton = document.getElementById('cancelButton-transition');
+    edgeSaveButton.onclick = null;
+    edgeCancelButton.onclick = null;
+    var edge_div = document.getElementById('transition-popUp');
+    edge_div.style.display = 'none';
+}
+
+//A1
+function edgeSaveData(data,callback){
+    var edgeLabelInput = document.getElementById('transition-label');
+    data.label = edgeLabelInput.value;
+    if(edgeLabelInput.length > 1){
+      alert("Solo se permite ingresar un caracter.");
+      return null;
+    }
+    else{
+      if(alfabeto1.includes(data.label)){
+        arrayEventos.push({from:data.from, to:data.to, evento:data.label});
+      }
+      else{
+        alert("El evento ingresado no pertenece al alfabeto. Intente nuevamente.");
+        return null;
+      }
+      edgeClearPopUp();
+      callback(data);
+    }
+}
+
+function nada(){
+  alert("Esta función aún no esta lista");
+}
